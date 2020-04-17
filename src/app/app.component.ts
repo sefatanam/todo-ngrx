1ba31c4786760922cac4347787c2c81468dba94d
Sefat Anam <sefatanam@gmail.com>
2020-04-17 15:47:49 +0600

    design ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   title = 'todo-ngrx';
+// }
+import { Todo } from './todo';
 import { Component } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'todo-ngrx';
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {}
+
+  public addTodo(): void {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  public toggleTodoComplete(todo): void {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  public removeTodo(todo): void {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  public allTodos(): number {
+    return this.incompleteTodos.length + this.completeTodos.length;
+  }
+  public get incompleteTodos(): Array<Todo> {
+    return this.todoDataService.getIncompleteTodos();
+  }
+
+  public get completeTodos(): Array<Todo> {
+    return this.todoDataService.getCompleteTodos();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,MatInputModule,MatToolbarModule,MatButtonModule,MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
